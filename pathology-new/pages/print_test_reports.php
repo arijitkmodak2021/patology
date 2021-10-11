0a87cae04a008a4e551f681303ab92f6e28899c8
arijitkmodak2021 <90644493+arijitkmodak2021@users.noreply.github.com>
2021-10-11 08:42:46 +0530

    pdf css

--- pathology-new/pages/print_test_reports.php
+++ pathology-new/pages/print_test_reports.php
@@ -1,20 +1,18 @@
 <?php
 	include("includes/config.php");
 	
-	if (!isset($_SESSION['is_logged_in']) or ($_SESSION['is_logged_in'] == ''))
-	{
+	if (!isset($_SESSION['is_logged_in']) or ($_SESSION['is_logged_in'] == '')) {
 	    header("Location:".$site_url."login");
 	}
-	elseif (!isset($_REQUEST['report_id']) or ($_REQUEST['report_id'] == ''))
-	{
+	elseif (!isset($_REQUEST['report_id']) or ($_REQUEST['report_id'] == '')) {
 	    header("Location:".$site_url."test-reports");
 	}
 	
 	$report_id				= (isset($_REQUEST['report_id']) && !empty($_REQUEST['report_id'])) ? $_REQUEST['report_id'] : 0;
 	$patien_report_details_sql	= mysqli_query($link, "SELECT * FROM patient_tests where id = '".$report_id."';");
 	$patien_report_details_arr 	= mysqli_fetch_all($patien_report_details_sql, MYSQLI_ASSOC);
-	//echo '<pre>';print_r($patien_report_details_arr); echo '</pre>';
-	$test_main_categories		= explode(',',$patien_report_details_arr[0]['test_main_categories']);
+	$test_main_categories		= explode(', ',$patien_report_details_arr[0]['test_main_categories']);
+	//echo '<pre>';print_r($test_main_categories); echo '</pre>';
 	
 	$patient_id				= $patien_report_details_arr[0]['p_id'];
 	$patien_details_sql			= mysqli_query($link, "SELECT * FROM patient_details where id = '".$patient_id."';");
@@ -22,13 +20,40 @@
 	
 	$report_values_sql			= mysqli_query($link, "select * from patient_report where report_id = '".$report_id." order by cat_grp_name asc, main_cat_name asc, test_type_name asc';");
 	$report_values_arr			= mysqli_fetch_all($report_values_sql, MYSQLI_ASSOC);
+	
+	$final_gen_report 			= [];
+	$each_cat_gen_report		= [];
+	$i3 						= 0;
+	$i4						= 0;
+	
+	for ($i2 = 0; $i2 < count($report_values_arr); $i2++) {
 		
-	$report_cat_values_sql		= mysqli_query($link, "select main_cat_name from patient_report where report_id = '".$report_id."' group by main_cat_name order by main_cat_name asc;");
-	$report_cat_values_arr		= mysqli_fetch_all($report_cat_values_sql, MYSQLI_ASSOC);	
+		if($final_gen_report[$i3]['cat_grp_name'] == $report_values_arr[$i2]['cat_grp_name']) {
+			
+			if($final_gen_report[$i3]['test_values'][$i4]['main_cat_name'] 	== $report_values_arr[$i2]['main_cat_name']){
+				$final_gen_report[$i3]['test_values'][$i4]['values'][]		= $report_values_arr[$i2];
+			}
+			else{
+				$i4++;
+				$final_gen_report[$i3]['test_values'][$i4]['main_cat_name']	= $report_values_arr[$i2]['main_cat_name'];
+				$final_gen_report[$i3]['test_values'][$i4]['values'][]		= $report_values_arr[$i2];
+			}
+			
+		}
+		else {
+			$i3++;
+			$final_gen_report[$i3]['cat_grp_name']					= $report_values_arr[$i2]['cat_grp_name'];
+			$final_gen_report[$i3]['test_values'][$i4]['main_cat_name']	= $report_values_arr[$i2]['main_cat_name'];
+			$final_gen_report[$i3]['test_values'][$i4]['values'][]		= $report_values_arr[$i2];
+			
+		}
+	}
+	
+	foreach($final_gen_report as $final_report) {
+		
 ?>
-
 <!-- Forms Section-->
-<section class="tables" style="padding-top: 30px;">   
+<section pos="<?php echo $i; ?>" class="tables" style="padding-top: 30px;">   
 	<div class="container-fluid report_gen">
 		<div class="row gy-4">
 			<div class="col-lg-12">
@@ -76,70 +101,74 @@
 									</div>
 								</div>
 							</div>
+							
 							<div class="row patient_rpt_sec" style="margin: 0;">
 								<?php
-									$sec_type_1	= [];
-									$sec_type_2	= [];
-									$sec_type_3	= [];
 									
-									$i = 0; $j = 1;
-									//echo count($report_cat_values_arr);
-									$do_split 	= (count($report_cat_values_arr) > 3) ? 1 : 0;
+									$cat_grp_name 	= $report_value['cat_grp_name'];
+									$do_split 	= (count($final_report['test_values']) > 3) ? 1 : 0;
 									$in_style 	= ($do_split == 1) ? '': 'float: none; margin: 0 auto; padding : 0';
 									$in_class 	= ($do_split == 1) ? 'col-sm-6 split': 'col-sm-12';
 									$border_right 	= ($do_split == 1) ? 'border-right: 2px solid #bababa;': '';
-									$elb_class	= ($do_split == 1) ? 'border_new_under': '';
-									
-									echo '<div class="'.$in_class.'" style="'.$in_style.'">';
-										if($do_split == 1)
-											echo '<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>
-												<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>';
-										else
-											echo '<p style="'.$border_right.' margin: 0; height: 15px;">&nbsp;</p>';
-												
-										foreach($report_values_arr as $report_value) {
-												
-											$cat_grp_name 	= $report_value['cat_grp_name'];
-											$main_cat_name	= $report_value['main_cat_name'];
-											$test_name	= $report_value['test_type_name'];
-												
-											$normal_range 	= ($report_value['normal_range'] != '') ? ucwords(strtolower($report_value['normal_range'])) : '';
-											$unit		= ($report_value['test_type_unit'] != '') ? '('.ucwords(strtolower($report_value['test_type_unit'])).')' : '';
-												
-											if($i == 0)
-												echo '<p style="'.$border_right.' margin: 0; height: 10px;"><b>'.ucwords(strtolower($main_cat_name)).':-</b></p>';
-											else{
-												$prev_cat_name	= $report_values_arr[$i - 1]['main_cat_name'];
-												if($main_cat_name != $prev_cat_name) {
-													$j++;
-													if((($j % 3) == 0) && ($do_split == 1))
-														echo '</div>
-																<div class="'.$in_class.'">
-																	<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>
-																	<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>';
-													
-													if($do_split == 1)
-														echo '<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>
-															<p style="'.$border_right.' margin: 0; height: 10px;"><b>'.ucwords(strtolower($main_cat_name)).':-</b></p>
-															<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>';
-													else
-														echo '<br>
-														<p style="'.$border_right.'margin: 0; height: 10px;"><b>'.ucwords(strtolower($main_cat_name)).':-</b></p>';
-												}
-											}
-												
-											echo '<div class=" each_print_div '.$elb_class.'">
-													<div class="child item font-small-14">'.ucwords(strtolower($test_name)).' :</div>
-													<div class="child item item_mid ">'.ucwords(strtolower($report_value['result_value'])).'</div>
-													<div class="child item font-small-13">'.$normal_range.' '.$unit.'</div>
-													<div style="clear: both"></div>
-												</div>';
-												
-												
-											$i++;
-										}
+									$elb_class	= ($do_split == 1) ? 'border_new_under': '';	
+										
+									foreach($final_report['test_values'] as $each_report_value) {
 											
-									echo '</div>'
+										$main_cat_name	= $each_report_value['main_cat_name'];
+											
+										$i = 0; $j = 1;
+										
+										foreach($each_report_value['values'] as $report_value) {
+											//echo '<pre>'; print_r($report_value); echo '</pre>'; die;
+											$test_name	= $report_value['test_type_name'];
+											
+											echo '<div class="'.$in_class.'" style="'.$in_style.'">';
+												if($do_split == 1)
+													echo '<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>
+														<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>';
+												else
+													echo '<p style="'.$border_right.' margin: 0; height: 15px;">&nbsp;</p>';
+														
+													$normal_range 	= ($report_value['normal_range'] != '') ? ucwords(strtolower($report_value['normal_range'])) : '';
+													$unit		= ($report_value['test_type_unit'] != '') ? '('.ucwords(strtolower($report_value['test_type_unit'])).')' : '';
+														
+													if($i == 0)
+														echo '<p style="'.$border_right.' margin: 0; height: 10px;"><b>'.ucwords(strtolower($main_cat_name)).':-</b></p>';
+													//else{
+													//	$prev_cat_name	= $report_values_arr[$i - 1]['main_cat_name'];
+													//	if($main_cat_name != $prev_cat_name) {
+													//		$j++;
+													//		if((($j % 3) == 0) && ($do_split == 1))
+													//			echo '</div>
+													//					<div class="'.$in_class.'">
+													//						<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>
+													//						<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>';
+													//		
+													//		if($do_split == 1)
+													//			echo '<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>
+													//				<p style="'.$border_right.' margin: 0; height: 10px;"><b>'.ucwords(strtolower($main_cat_name)).':-</b></p>
+													//				<p style="'.$border_right.' margin: 0; height: 10px;">&nbsp;</p>';
+													//		else
+													//			echo '<br>
+													//			<p style="'.$border_right.'margin: 0; height: 10px;"><b>'.ucwords(strtolower($main_cat_name)).':-</b></p>';
+													//	}
+													//}
+														
+													echo '<div class=" each_print_div '.$elb_class.'">
+															<div class="child item font-small-14">'.ucwords(strtolower($test_name)).' :</div>
+															<div class="child item item_mid ">'.ucwords(strtolower($report_value['result_value'])).'</div>
+															<div class="child item font-small-13">'.$normal_range.' '.$unit.'</div>
+															<div style="clear: both"></div>
+														</div>';
+														
+														
+													$i++;
+												
+	
+												
+											echo '</div>';
+										}
+									}
 								?>
 							</div>
 						</div>
@@ -176,3 +205,5 @@
 		</div>
 	</div>
 </section>
+
+<?php } echo 'loop end' ?>
